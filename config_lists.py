bd0440c3fe3a1f764aa90fa4f3ff2098ed95b1eb
aphyer1992 <hyerandrew@gmail.com>
2025-07-19 10:15:59 -0400

    weapons

--- config_lists.py
+++ config_lists.py
@@ -134,7 +134,7 @@ animal_companions = [
 
 energy_types = ['Acid', 'Cold', 'Electricity', 'Fire']
 
-weapons = [
+weapons_raw = [
     ['Dagger', 'Light', 'Simple', 'PS', 1],
     ['Light Mace', 'Light', 'Simple', 'B', 1],
     ['Sickle', 'Light', 'Simple', 'S', 0.2],
@@ -150,4 +150,64 @@ weapons = [
     ['Darts', 'Thrown', 'Simple', 'P', 0.2],
     ['Javelins', 'Thrown', 'Simple', 'P', 1],
     ['Sling', 'Ranged', 'Simple', 'B', 0.5],
+    ['Throwing Axes', 'Thrown', 'Martial', 'S', 0.5],
+    ['Handaxe', 'Light', 'Martial', 'S', 1],
+    ['Kukri', 'Light', 'Martial', 'P', 0.5],
+    ['Shortsword', 'Light', 'Martial', 'P', 1],
+    ['Battleaxe', 'One-Handed', 'Martial', 'S', 1],
+    ['Flail', 'One-Handed', 'Martial', 'B', 1],
+    ['Longsword', 'One-Handed', 'Martial', 'S', 1],
+    ['Rapier', 'One-Handed', 'Martial', 'P', 1],
+    ['Scimitar', 'One-Handed', 'Martial', 'S', 1],
+    ['Trident', 'One-Handed', 'Martial', 'P', 1],
+    ['Warhammer', 'One-Handed', 'Martial', 'B', 1],
+    ['Falchion', 'Two-Handed', 'Martial', 'S', 1],
+    ['Glaive', 'Two-Handed', 'Martial', 'S', 1],
+    ['Greataxe', 'Two-Handed', 'Martial', 'S', 1],
+    ['Greatsword', 'Two-Handed', 'Martial', 'S', 1],
+    ['Greatclub', 'Two-Handed', 'Martial', 'B', 1],
+    ['Heavy Flail', 'Two-Handed', 'Martial', 'B', 1],
+    ['Halberd', 'Two-Handed', 'Martial', 'S', 1],
+    ['Scythe', 'Two-Handed', 'Martial', 'S', 0.5],
+    ['Longbow', 'Ranged', 'Martial', 'P', 1],
+    ['Shortbow', 'Ranged', 'Martial', 'P', 1],
+    ['Sai', 'Light', 'Exotic', 'P', 0.5],
+    ['Bastard Sword', 'One-Handed', 'Exotic', 'S', 1],
+    ['Dwarven Waraxe', 'One-Handed', 'Exotic', 'S', 1],
+    ['Kama', 'Light', 'Exotic', 'S', 0.5],
+    ['Nunchaku', 'Light', 'Exotic', 'B', 0.5],
+    ['Siangham', 'Light', 'Exotic', 'P', 0.5],
+    ['Spiked Chain', 'Two-Handed', 'Exotic', 'P', 0.5],
+    ['Whip', 'Two-Handed', 'Exotic', 'S', 0.2],
+    ['Orc Double Axe', 'Two-Handed', 'Exotic', 'S', 0.5],
+    ['Gnome Hooked Hammer', 'One-Handed', 'Exotic', 'BP', 0.2],
+    ['Dire Flail', 'Two-Handed', 'Exotic', 'B', 0.5],
+    ['Bolas', 'Thrown', 'Exotic', 'B', 0.2],
+    ['Hand Crossbow', 'Ranged', 'Exotic', 'P', 0.5],
+    ['Repeating Heavy Crossbow', 'Ranged', 'Exotic', 'P', 0.2],
+    ['Repeating Light Crossbow', 'Ranged', 'Exotic', 'P', 0.2],
+    ['Net', 'Thrown', 'Exotic', 'B', 0.2],
+    ['Shuriken', 'Thrown', 'Exotic', 'P', 0.2],
+]
+
+weapon_details = {}
+for w in weapons_raw:
+    weapon_details[w[0]] = {
+        'name': w[0],
+        'category' : w[1],
+        'proficiency': w[2],
+        'damage_type': w[3],
+        'frequency': w[4],
+        'metal' : False if w[0] in ('Quarterstaff', 'Club', 'Greatclub', 'Net', 'Bolas') or w[1] == 'Ranged' else True,
+    }
+
+random_weapon_selector = WeightedChoice(
+    [w[0] for w in weapons_raw],
+    [w[4] for w in weapons_raw]
+)
+
+special_weapon_materials = [
+    'Adamantine',
+    'Cold Iron',
+    'Silver',
 ]
--- treasure.py
+++ treasure.py
@@ -1,11 +1,45 @@
+import random
 from weighted_choice import WeightedChoice
+from config_lists import random_weapon_selector, magic_schools, domains, energy_types, creature_types, animal_companions, weapon_details, special_weapon_materials
 
+minor_specific_weapon_gen = WeightedChoice([
+    'Sleep Arrow',
+    'Screaming Bolt',
+    'Javelin of Lightning',
+    'Slaying Arrow',
+    lambda weapon: None if weapon_details[weapon]['metal'] == False else 'Masterwork ' + random.choice(special_weapon_materials) + ' ' + weapon,
+], [1, 1, 1, 1, 10])
+
+minor_magic_weapon_enchantment_gen = WeightedChoice([
+    lambda weapon: random.choice(creature_types) + ' Bane',
+    'Defending', 'Flaming', 'Frost', 'Shock', 'Ghost Touch',
+    lambda weapon: None if weapon_details[weapon]['damage_type'] == 'B' else 'Keen',
+    'Ki Focus', 'Merciful', 'Mighty Cleaving', 'Spell Storing', 'Throwing', 'Thundering', 'Vicious',
+    lambda weapon: minor_magic_weapon_enchantment_gen.choose(weapon) + ', ' + minor_magic_weapon_enchantment_gen.choose(weapon),
+], [10, 7, 10, 10, 10, 9, 11, 4, 4, 7, 5, 4, 4, 4, 1])
+
+minor_magic_weapon_gen = WeightedChoice([
+    lambda weapon: "+1 " + weapon,
+    lambda weapon: "+2 " + weapon,
+    lambda weapon: minor_specific_weapon_gen.choose(weapon),
+    lambda weapon: minor_magic_weapon_gen.choose(weapon) + ", " + minor_magic_weapon_enchantment_gen.choose(weapon),
+], [70, 15, 5, 10])
+
+minor_magic_armor_gen=None
+minor_magic_ring_gen=None
+minor_magic_scroll_gen=None
+minor_magic_wand_gen=None
+minor_magic_potion_gen=None
+minor_magic_wondrous_gen=None
 minor_magic_item_gen = WeightedChoice([
-    minor_magic_weapon_gen,
+    lambda weapon: minor_magic_weapon_gen(random_weapon_selector.choose()).choose(),
     minor_magic_armor_gen,
     minor_magic_potion_gen,
     minor_magic_ring_gen,
     minor_magic_scroll_gen,
     minor_magic_wand_gen,
     minor_magic_wondrous_gen,
-], [4, 5, 35, 2, 35, 10, 9]
+], [4, 5, 35, 2, 35, 10, 9])
+
+for i in range(10):
+    print(minor_magic_weapon_gen.choose(random_weapon_selector.choose()))
